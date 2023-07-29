301865631b1b25226eb7cb8cd5d92df322338a08
Thaddeus Diamond <thaddeus.diamond@gmail.com>
2023-07-28 23:13:59 -0500

    Cron (update): Skip failures and reduce total searched

--- src/app/api/cron/update/route.js
+++ src/app/api/cron/update/route.js
@@ -4,19 +4,27 @@ import prisma from '../../../../prisma/prisma.mjs';
 
 import { DEFAULT_ORDER_API, UNPAID } from '../../../../components/ordinalsbot/config.js';
 
+const TWO_DAY_MS = 48 * 60 * 60 * 1000;
+
 export async function GET() {
   try {
+    const oldestTime = new Date(new Date().getTime() - ONE_DAY_MS);
+    console.log(`Retrieving unpaid orders since ${oldestTime}`);
+
     const unpaidOrders = await prisma.order.findMany({
       where: {
-        status: UNPAID
+        status: UNPAID,
+        createdAt: { gt: oldestTime }
       }
     });
 
     var updatedOrders = 0;
     for (const unpaidOrder of unpaidOrders) {
+      console.log(`Retrieving status for "${unpaidOrder.id}"`);
       const unpaidOrderStatusReq = await fetch(`${DEFAULT_ORDER_API}?id=${unpaidOrder.id}`);
       if (unpaidOrderStatusReq.status !== 200) {
         console.error(`Could not retrieve order status for order "${unpaidOrder.id}" (${unpaidOrderStatusReq.status}): ${unpaidOrderStatusReq.statusText}`);
+        continue;
       }
       const unpaidOrderStatus = await unpaidOrderStatusReq.json();
       const newOrderStatus = unpaidOrderStatus.charge.status;
