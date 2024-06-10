b50d43c15a22c6b92c717002b273f690b4f94e5b
Thaddeus Diamond <thaddeus.diamond@gmail.com>
2024-06-10 13:47:26 -0500

    fix(Logs): Add logging for discounts
    The API for MagicEden changed, causing catastrophic errors and letting
    everyone build for free.  This change corrects that, and adds logging in
    the future in case ME's API changes.
    
    [ Testing: test in prod ]

--- src/utils/price.js
+++ src/utils/price.js
@@ -4,7 +4,7 @@ export const DEFAULT_PRICE = 10000;
 export const BTC_TO_SATS = 100000000;
 
 function getCollectionUrl(symbol, owner) {
-  return `https://api-mainnet.magiceden.dev/v2/ord/btc/tokens?collectionSymbol=${symbol}&ownerAddress=${owner}&showAll=true&sortBy=priceAsc`;
+  return `https://api-mainnet.magiceden.dev/v2/ord/btc/tokens?collectionSymbol=${symbol}&ownerAddress=${owner}&showAll=true&sortBy=priceAsc&limit=100`;
 }
 
 export async function getFee(buyerAddress) {
@@ -18,7 +18,10 @@ export async function getFee(buyerAddress) {
       const discountResult = await fetch(getCollectionUrl(collectionDiscount.symbol, buyerAddress), {
         headers: discountHeaders
       }).then(res => res.json());
-      const collectionDiscountResult = collectionDiscount.pct * discountResult.total;
+      const collectionDiscountResult = collectionDiscount.pct * discountResult.tokens.length;
+      if (collectionDiscountResult === NaN) {
+        console.error(`ME API IS RETURNING BOGUS DATA, CHECK FOR A CHANGE: ${discountResult}`);
+      }
       console.log(`${buyerAddress} has a discount of ${collectionDiscountResult * 100}% from "${collectionDiscount.symbol}" holdings`);
       existingDiscount += collectionDiscountResult;
     }
